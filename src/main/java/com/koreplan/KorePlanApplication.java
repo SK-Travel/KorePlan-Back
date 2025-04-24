61247f155bc50bc5f5d45b6689bba7a6b3e7d606
HYUNWOO\hw000 <hw0005@naver.com>
2025-04-24 13:22:25 +0900

    first

---
+++ src/main/java/com/koreplan/KorePlanApplication.java
@@ -0,0 +1,13 @@
+package com.koreplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorePlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KorePlanApplication.class, args);
+	}
+
+}
