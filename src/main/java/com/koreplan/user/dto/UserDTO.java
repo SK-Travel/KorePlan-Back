d02092520c3a4e07c85340918059a0272f54be5c
HYUNWOO\hw000 <hw0005@naver.com>
2025-05-02 16:31:05 +0900

    2025_05_02_(1)

--- src/main/java/com/koreplan/user/dto/UserDTO.java
+++ src/main/java/com/koreplan/user/dto/UserDTO.java
@@ -12,12 +12,18 @@ public class UserDTO {
 	@Autowired
 	private UserRepository userRepository;
 	
-	// 유저 조회
+	// 유저 조회 테스트용
 	public UserEntity getUserEntityById(int id) {
 		return userRepository.findById(id).orElse(null);
 	}
 	
-	// 비밀번호 조회
+	// 회원가입용 아이디 중복확인 조회
+	public UserEntity getUserEntityByLoginId(String loginId) {
+		return userRepository.findByLoginId(loginId).orElse(null);
+	}
+	
+	
+	// 회원수정용 비밀번호 존재여부 조회
 	public UserEntity getEntityByLoginId(String loginId) {
 		return userRepository.findByLoginId(loginId).orElse(null);
 	}
