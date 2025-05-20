7f395c446d630930ce600e305e3353f4a37c63d2
presenteeL <leejh3940@skuniv.ac.kr>
2025-05-20 14:13:42 +0900

    created category api service

--- src/main/java/com/koreplan/category/entity/CategoryEntity.java
+++ src/main/java/com/koreplan/category/entity/CategoryEntity.java
@@ -1,5 +1,27 @@
 package com.koreplan.category.entity;
 
-public class CategoryEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Table(name="categories")
+@Getter
+@Setter
+public class CategoryEntity {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String lclsSystm1Cd;
+    private String lclsSystm1Nm;
+    private String lclsSystm2Cd;
+    private String lclsSystm2Nm;
+    private String lclsSystm3Cd;
+    private String lclsSystm3Nm;
+    private int rnum;
 }
