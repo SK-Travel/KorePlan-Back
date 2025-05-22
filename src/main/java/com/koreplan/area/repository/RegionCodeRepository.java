d6e5439775dd0ba41dafd64a3142a665e7017a1f
presenteeL <leejh3940@skuniv.ac.kr>
2025-05-22 18:31:45 +0900

    pull

--- src/main/java/com/koreplan/area/repository/RegionCodeRepository.java
+++ src/main/java/com/koreplan/area/repository/RegionCodeRepository.java
@@ -1,9 +1,11 @@
 package com.koreplan.area.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.koreplan.area.entity.RegionCodeEntity;
 
 public interface RegionCodeRepository extends JpaRepository<RegionCodeEntity,Long> {
-	
+	 Optional<RegionCodeEntity> findByRegioncode(Long regioncode);
 }
--- src/main/java/com/koreplan/area/repository/WardCodeRepository.java
+++ src/main/java/com/koreplan/area/repository/WardCodeRepository.java
@@ -1,9 +1,12 @@
 package com.koreplan.area.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.koreplan.area.entity.RegionCodeEntity;
 import com.koreplan.area.entity.WardCodeEntity;
 
 public interface WardCodeRepository extends JpaRepository <WardCodeEntity,Long> {
-
+	Optional<WardCodeEntity> findByWardcodeAndRegionCodeEntity(Long wardcode, RegionCodeEntity region);
 }
